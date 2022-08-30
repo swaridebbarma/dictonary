3235ed241c1977ef98895bdc2a8846ff083d5b40
swaridebbarma <98303710+swaridebbarma@users.noreply.github.com>
2022-08-30 09:04:19 +0530

    Add files via upload

---
+++ doc_qs44.py
@@ -0,0 +1,15 @@
+def list_of_dicts(marks):
+    keys = marks.keys()
+    vals = zip(*[marks[k] for k in keys])
+    result = [dict(zip(keys, v)) for v in vals]
+    return result
+
+marks = {'Science': [88, 89, 62, 95], 'Language': [77, 78, 84, 80]}
+print("Original dictionary of lists:")
+print(marks)
+print("\nSplit said dictionary of lists into list of dictionaries:")
+print(list_of_dicts(marks))
+# 44 no
+
+
+# a={'Science': [88, 89, 62, 95], 'Language': [77, 78, 84, 80]}
