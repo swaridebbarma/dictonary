da026bfd15cdda25286cf465febfaa20ad3678ed
swaridebbarma <98303710+swaridebbarma@users.noreply.github.com>
2022-08-30 09:20:40 +0530

    Add files via upload

---
+++ doc_qs19.py
@@ -0,0 +1,52 @@
+data = {'id1':
+
+   {'name': ['Sara'],
+
+    'class': ['V'],
+
+    'subject_integration': ['english, math, science']
+
+   },
+
+ 'id2':
+
+  {'name': ['David'],
+
+    'class': ['V'],
+
+    'subject_integration': ['english, math, science']
+
+   },
+
+ 'id3':
+
+    {'name': ['Sara'],
+
+    'class': ['V'],
+
+    'subject_integration': ['english, math, science']
+
+   },
+
+ 'id4':
+
+   {'name': ['Surya'],
+
+    'class': ['V'],
+
+    'subject_integration': ['english, math, science']
+
+   },
+
+}
+
+
+
+result = {}
+
+for key,value in data.items():
+    if value not in result.values():
+        result[key] = value
+
+print(result)
+# 19 no
