92fce6996020fe8109cd7e2d3726bf79319e41d3
swaridebbarma <98303710+swaridebbarma@users.noreply.github.com>
2022-08-30 09:05:08 +0530

    Add files via upload

---
+++ doc_qs42.py
@@ -0,0 +1,8 @@
+a={'Cierra Vega': 12, 'Alden Cantrell': 12, 'Kierra Gentry': 12, 'Pierre Cox': 12}
+n=int(input("enter no:-"))
+for i in a.values():
+    if i==n:
+        print("true")
+    else:
+        print("false")
+# 42 no
