8759445fade7f1adb1f4cd1dea59265dc116c292
swaridebbarma <98303710+swaridebbarma@users.noreply.github.com>
2022-08-30 08:47:12 +0530

    Add files via upload

---
+++ revers.py
@@ -0,0 +1,66 @@
+# a=[1,2,3,1,2,1,6,5]
+# def fun (a):
+#     i=0
+#     c=0
+#     b=[]
+#     while i <len(a):
+#         if a[i] not in b:
+#             b.append(a[i])
+#             c+=1
+#         i+=1
+#     print(c,b)
+# fun(a)
+
+
+# # a="my name"
+# a=input("enter no")
+# i=-1
+# while i<len(a):
+#     b=(a[::-1])
+#     i+=1
+# print(b)
+
+# a="mom"
+# b=a[::-1]
+# if a==b:
+#     print("palindram")
+# else:
+#     print("not palindrom")
+    
+    
+# a=[1,2,3,1,2,1,6,5]
+# i=0
+# c=0
+# c2=0
+# b=[]
+# d=[]
+# while i<len(a):
+#     if a[i]== 1:
+#         b.append(a[i])
+#         c+=1
+#     else:
+#         d.append(a[i])
+#         c2+=1
+#     i+=1
+# print(c2,d)
+
+# 27 no
+a="i love you"
+i=0
+b=" "
+while i<len(a):
+    # print(a[i])
+    if a[i]==" ":
+        # print(a)
+        pass
+    else:
+        b+=a[i]
+    i=i+1
+print(b)
+
+
+
+
+    
+
+
