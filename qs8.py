383a09fbdb8f618679f491ce9a660ba95c7bd463
swaridebbarma <98303710+swaridebbarma@users.noreply.github.com>
2022-08-30 08:54:05 +0530

    Add files via upload

---
+++ qs8.py
@@ -0,0 +1,14 @@
+b={}
+for i in range(5):
+    name=input("enter name")
+    marks=int(input("enter no"))
+    b[name]=marks
+print(b)
+"""b={}
+i=1
+while i<=10:
+    name=input("enter name")
+    marks=int(input("enter your marks"))
+    b[name]=marks
+    i=i+1
+print(b)"""
