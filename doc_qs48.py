929b3e718489c203f6b3213c4c7da7cf103ce676
swaridebbarma <98303710+swaridebbarma@users.noreply.github.com>
2022-08-30 09:01:32 +0530

    Add files via upload

---
+++ doc_qs48.py
@@ -0,0 +1,32 @@
+# def test(dictt):
+#     result = {}
+#     for val in dictt.values(): 
+#         result[val] = len(val) 
+#     return result    
+
+# color_dict = {1 : 'red', 2 : 'green', 3 : 'black', 4 : 'white', 5 : 'black'}
+# print("\nOriginal Dictionary:")
+# print(color_dict)
+# print("Length of dictionary values:")
+# print(test(color_dict))
+
+# color_dict = {'1' : 'Austin Little', '2' : 'Natasha Howard', '3' : 'Alfred Mullins', '4' : 'Jamie Rowe'}
+# print("\nOriginal Dictionary:")
+# print(color_dict)
+# print("Length of dictionary values:")
+# print(test(color_dict))
+
+
+
+c= {1 : 'red', 2 : 'green', 3 : 'black', 4 : 'white', 5 : 'black'}
+b={}
+for i in c.values():
+    b[i]=len(i)
+print(b)
+c = {'1' : 'Austin Little', '2' : 'Natasha Howard', '3' : 'Alfred Mullins', '4' : 'Jamie Rowe'}
+b={}
+for i in c.values():
+    b[i]=len(i)
+print(b)
+
+# 48 no
