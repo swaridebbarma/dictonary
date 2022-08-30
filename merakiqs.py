0574978f8db8732c07bf4c57e4d7e50e854a6404
swaridebbarma <98303710+swaridebbarma@users.noreply.github.com>
2022-08-30 08:57:29 +0530

    Add files via upload

---
+++ merakiqs.py
@@ -0,0 +1,17 @@
+"""thisdict =	{"brand": "Ford","model": "Mustang","year": 1964}
+for x in thisdict.values():
+  print(x)"""
+  
+  
+"""x={"brand": "Ford",
+  "model": "Mustang",
+  "year": 2020}
+a=x.setdefault("brand","toyota")
+print(a)"""
+
+dict={"brand":"ford",
+   "model":"mustang",
+   "year":2020
+}
+x=dict.setdefault("place","new delhi")
+print(x)
