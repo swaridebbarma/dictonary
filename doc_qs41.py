c549a95ccaa1020001033a07404f3ff71c50bbe7
swaridebbarma <98303710+swaridebbarma@users.noreply.github.com>
2022-08-30 09:06:27 +0530

    Add files via upload

---
+++ doc_qs41.py
@@ -0,0 +1,9 @@
+def filter_data(students):
+    result = dict(filter(lambda x: (x[1][0], x[1][1]) > (6.0, 70), students.items()))
+    return result  
+students = {'Cierra Vega': (6.2, 70), 'Alden Cantrell': (5.9, 65), 'Kierra Gentry': (6.0, 68), 'Pierre Cox': (5.8, 66)}
+print("Original Dictionary:")
+print(students)
+print("\nHeight> 6ft and Weight> 70kg:")
+print(filter_data(students))
+# 41 no
